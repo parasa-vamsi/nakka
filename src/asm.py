1d37fcb318beeea752d5118006db43c33a77ce88
parasa-vamsi <parasa.vamsi@gmail.com>
2025-11-27 11:19:18 -0800

    refactor code: X86AsmUtils

---
+++ src/asm.py
@@ -0,0 +1,23 @@
+class X86AsmUtils:
+
+    def __init__(self):
+        self.code = ""
+        self.emit_header()
+
+    def emit_header(self):
+        self.code += "DEFAULT REL" + "\n"
+        self.code += "SECTION .text" + "\n"
+        self.code += "\t \t" + "global _entry" + "\n"
+        self.emit_label("_entry")
+
+    def wrapup(self):
+        self.code += "\t \t" + "ret" + "\n\n" + "section .note.GNU-stack noexec"
+
+    def emit_instr(self, instr):
+        self.code += "\t \t" + f"{instr}" + "\n"
+
+    def emit_label(self, label):
+        self.code += f"{label}:" + "\n"
+
+    def show(self):
+        print(self.code)
