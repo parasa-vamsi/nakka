22545d09322b4a9e378ed921854629a69145e847
parasa-vamsi <parasa.vamsi@gmail.com>
2025-12-17 01:27:17 -0800

    Optimize if statement to efficiently handle missing else block

--- src/compiler.py
+++ src/compiler.py
@@ -4,15 +4,14 @@ import src.asm as x86
 # Keep this formatting to avoid IndentationError
 program = \
 """
-x = 3
-y = 3
+def f(x):
+    if x > 5:
+        return 55
+    # else:
+    return -55
 
-if x > 5 or y > 2 :
-    x += 1
-else:
-    x -= 1
-
-x
+z = f(6)
+z
 
 """
 
@@ -296,20 +295,25 @@ class Compiler:
                 asm.emit_label(f'{label_done}')
 
             #------------------- if statement -----------------
-            case AST.If(test=if_exp, body=then_exp, orelse=else_exp):
+            case AST.If(test=if_exp, body=then_block, orelse=else_block):
                 print('Compiling If Statement')
                 asm.emit_comment('if-statement')
                 compile_ast(if_exp, env)
                 asm.emit_instr('cmp rax, 0')
-                label_else = gen_sym('ifstmt_else')
-                asm.emit_instr(f'je {label_else}')
-                for stmt in then_exp:
-                    compile_ast(stmt, env)
                 label_done = gen_sym('ifstmt_done')
-                asm.emit_instr(f'jmp {label_done}')
-                asm.emit_label(f'{label_else}')
-                for stmt in else_exp:
+                if else_block:
+                    label_else = gen_sym('ifstmt_else')
+                    asm.emit_instr(f'je {label_else}')
+                else:
+                    asm.emit_instr(f'je {label_done}')
+
+                for stmt in then_block:
                     compile_ast(stmt, env)
+                if else_block:
+                    asm.emit_instr(f'jmp {label_done}')
+                    asm.emit_label(f'{label_else}')
+                    for stmt in else_block:
+                        compile_ast(stmt, env)
                 asm.emit_label(f'{label_done}')
 
             #------------------- Variable binding -----------------
